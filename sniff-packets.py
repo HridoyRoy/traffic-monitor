e8bd5e87a33325ca0373c2828fee7fb2292a33bb
HridoyRoy <hridoyroy@berkeley.edu>
2020-06-16 13:36:36 -0700

    some preliminary stats gathering

--- sniff-packets.py
+++ sniff-packets.py
@@ -1,4 +1,6 @@
 import threading
+import os
+import json
 from scapy.all import *
 from scapy.layers.http import HTTPRequest # import HTTP packet
 # from scapy import *
@@ -15,7 +17,10 @@ LOG_FILENUM = 0
 LOG_FILENAME = "./saved/log"
 LOG_LINE_READ = 0
 LOG_LINE_WRITE = 0
-STATS_FILE = "./saved/stats"
+STATS_FILE = "./saved/stats.txt"
+INIT_STATS = {"most_hits_section" : "", "some_stat" : 5}
+INIT_SECTION_HITS = {}
+INIT_REVERSE_SECTION_HITS = {}
 
 def sniff_packets(iface=None):
     """
@@ -65,16 +70,77 @@ def process_packet(packet):
     if not logfile.closed:
         logfile.close()
 
+# Create new log file and set all global values appropriately
 def reset_logfile():
     print("not yet boyo")
 
 '''
 Open log file and process from line saved_line_num and filename, along with other stats collected in stats file. Update saved_line_num and if saved_line_num is too large, open new file and change file name. 
+
+This function assumes a statistics file is already initialized, so it is contingent on init_statistics being run.. 
 '''
 def aggregate_statistics():
     threading.Timer(10.0, aggregate_statistics).start()
-    print("nothing for now")
+    global LOG_FILENUM
+    global LOG_FILENAME
+    global LOG_LINE_READ
+    global INIT_STATS
+    global INIT_SECTION_HITS
+    global INIT_REVERSE_SECTION_HITS
+    # open stats (come back to this later, as for now you can just store in mem)
+    
+    # open log
+    log_filename = LOG_FILENAME + str(LOG_FILENUM) + ".txt"
+    with open(log_filename) as log_file:
+        for i, line in enumerate(log_file):
+            if i == LOG_LINE_READ:
+                # read and update read counter, and add to appropriate log locations
+                log_vals = line.split(":")
+                url = log_vals[0]
+                if url in INIT_SECTION_HITS:
+                    num_hits = INIT_SECTION_HITS[url]
+                    INIT_REVERSE_SECTION_HITS[num_hits].remove(url)
+                    if INIT_REVERSE_SECTION_HITS[num_hits + 1]:
+                        INIT_REVERSE_SECTION_HITS[num_hits + 1].append(url)
+                    else:
+                        INIT_REVERSE_SECTION_HITS[num_hits + 1] = [url]
+                    INIT_SECTION_HITS[url] += 1
+                else:
+                    INIT_SECTION_HITS[url] = 1
+                    if INIT_REVERSE_SECTION_HITS[0]:
+                        INIT_REVERSE_SECTION_HITS[0] += [url]
+                    else:
+                        INIT_REVERSE_SECTION_HITS[0] = [url]
+                #  update read counter
+                LOG_LINE_READ += 1
 
+    INIT_STATS["most_hits_section"] = INIT_REVERSE_SECTION_HITS[INIT_REVERSE_SECTION_HITS.keys().max()][0]
+    
+    # display stats
+    
+    # TODO: Displaying full url hits here -- need to display and return stats only for sections, not full urls
+    print("most hits were for the following URL")
+    print(INIT_STATS["most_hits_section"])
+    
+    # write to stats
+    # TODO: We are saving the stats dicts, but not using the saved values anywhere. Tbd if we ever want to load the stats from memory in case the program crashes, or if we just want to begin again from scratch
+    with open("./saved/stats.txt", "w") as stats_out:
+        json.dump([INIT_STATS, INIT_SECTION_HITS, INIT_REVERSE_SECTION_HITS], stats_out)
+
+
+def init_statistics():
+    global INIT_STATS
+    global INIT_SECTION_HITS
+    global INIT_REVERSE_SECTION_HITS
+
+    stats_list = [INIT_STATS, INIT_SECTION_HITS, INIT_REVERSE_SECTION_HITS]
+    if not os.path.exists('./saved'):
+        os.makedirs('./saved')
+    if os.path.exists("./saved/stats.txt"):
+        os.remove("./saved/stats.txt")
+    with open("./saved/stats.txt", "w") as stats:
+        json.dump(stats_list, stats, indent=2)
+    return
 
 if __name__ == "__main__":
     import argparse
@@ -86,5 +152,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     iface = args.iface
     show_raw = args.show_raw
+    init_statistics()
     aggregate_statistics()
     sniff_packets(iface)
