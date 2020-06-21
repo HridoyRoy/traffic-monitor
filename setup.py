a7b89eca148dee7241ed94fd9068dd426c9fd0dc
HridoyRoy <hridoyroy@berkeley.edu>
2020-06-21 16:51:19 -0700

    cleanup and documentation

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.1',
     packages=['t_monitor',],
     license='Just kidding, there is no license here, but Hridoy Roy wrote this',
-    long_description=open('README.txt').read(),
+    long_description=open('writeup.txt').read(),
 )
